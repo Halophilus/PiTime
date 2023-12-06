ee174ea2b8d1413514d2a1a206eb6183022e85ec
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-12-05 23:04:45 -0500

    Add hardware test
    Sound test already passed

---
+++ test/hardware_test.py
@@ -0,0 +1,30 @@
+from gpiozero import LED, Buzzer
+from time import sleep
+
+# Initialize the LED and buzzer
+vibration = LED(25)
+buzzer = Buzzer(5)
+
+try:
+    while True:
+        vibration.on()
+        
+        sleep(1)
+        vibration.off()
+        buzzer.on()
+        
+        sleep(1)
+        buzzer.off()
+        vibration.on()
+
+        sleep(1)
+        vibration.off()
+        buzzer.on()
+        sleep(1)
+        buzzer.off()
+        sleep(5)
+
+except KeyboardInterrupt:
+    # Clean up the GPIOs on Ctrl+C exit
+    led.close()
+    buzzer.close()
